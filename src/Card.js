4f632966d3388e55da80e1964a3209cd6d1f71b4
Nika Tomadze <nika.tomadze@gmail.com>
2023-01-15 18:43:53 +0000

    Update Card.js and config.js

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,6 @@
-'use strict'
+"use strict";
 
-import { Flex, Button } from 'smbls'
+import { Flex, Button } from "smbls";
 
 /**
  * Creating a component
@@ -15,60 +15,61 @@ import { Flex, Button } from 'smbls'
  */
 export const Card = {
   extend: Flex,
-  state: { active: 0 },
 
-  /**
-   * Properties
-   * @tutorial https://docs.symbols.app/props
-   */
   props: {
-    flow: 'column',
-    align: 'flex-start space-between',
-    padding: 'A2 B2 B2 B2',
-    round: 'A2',
-    boxSize: 'auto G2-default',
+    flexDirection: "row",
 
-    '@dark': {
-      background: 'gray3 .65',
-      color: 'white',
-      boxShadow: 'black .05, 0 A A'
+    width: "100%",
+    padding: "B 0 B Z1",
+    height: "100px",
+    // minHeight: 'calc(10px + 10px)',
+
+    border: "lightBlack 1, solid",
+    borderWidth: "0 0 V 0",
+
+    willChange: "border-bottom-color",
+    transitionProperty: "all",
+    transition: "all",
+    transitionDuration: "150ms",
+    transitionTimingFunction: "ease",
+    transitionDelay: "0s",
+
+    "@mobile": {
+      padding: "0 0 0 0",
+      backgroundColor: "red",
+      height: "150px",
     },
-    '@light': {
-      background: 'white .8',
-      color: 'gray3',
-      boxShadow: 'gray12 .35, 0 A A'
-    }
-  },
 
-  /**
-   * Atoms
-   * @tutorial https://docs.symbols.app/atoms
-   */
-  caption: {
-    props: { color: 'gray 1 +85' },
-    text: 'Incremental Number'
-  },
-
-  title: {
-    tag: 'h2',
-    props: { margin: '0 0 B1' },
-    text: ({ state }) => state.active
-  },
-
-  button: {
-    extend: Button,
-    props: {
-      fontSize: 'A',
-      theme: 'primary',
-      padding: 'Z2 B2',
-      round: 'B2',
-      margin: '-X2'
+    "@tablet": {
+      padding: "0 0 0 0",
     },
-    text: 'Increment',
-    on: {
-      click: (ev, element, state) => {
-        state.update({ active: state.active + 1 })
-      }
-    }
-  }
-}
+
+    "@dark": {
+      borderColor: "lightBlack",
+    },
+
+    "@light": {
+      borderColor: "#CFCFD1",
+    },
+
+    ":hover": {
+      borderColor: "#CFCFD1 0.35",
+    },
+  },
+  vote: (props) => {
+    // if (props.data && props.passedFromComments) console.warn('Data is missing', props.data)
+    return {
+      extend: Vote,
+      props: {},
+      passed: props.passedFromComments,
+      data: props.data,
+    };
+  },
+  content: (props) => {
+    return {
+      extend: Content,
+      props: {},
+      data: props.data,
+    };
+  },
+};
--- src/config.js
+++ src/config.js
@@ -1,54 +1,53 @@
-'use strict'
+"use strict";
 
-import { init } from 'smbls'
+import { init } from "smbls";
 
-const S3_BUCKET = 'https://symbols-fonts.s3.us-west-1.amazonaws.com'
+const S3_BUCKET = "https://symbols-fonts.s3.us-west-1.amazonaws.com";
 
-const AvenirLight = '/avenir/49c5f3e1-5867-4b1d-a843-2d07fa60d85d.woff2'
-const AvenirRegular = '/avenir/2cd55546-ec00-4af9-aeca-4a3cd186da53.woff2'
-const AvenirMedium = '/avenir/627fbb5a-3bae-4cd9-b617-2f923e29d55e.woff2'
-const AvenirDemiBold = '/avenir/6afe4676-059a-4aa8-b891-29856bbcba22.woff2'
-const AvenirBold = '/avenir/14c73713-e4df-4dba-933b-057feeac8dd1.woff2'
-const AvenirHeavy = '/avenir/5c57b2e2-f641-421e-a95f-65fcb47e409a.woff2'
-
+const AvenirLight = "/avenir/49c5f3e1-5867-4b1d-a843-2d07fa60d85d.woff2";
+const AvenirRegular = "/avenir/2cd55546-ec00-4af9-aeca-4a3cd186da53.woff2";
+const AvenirMedium = "/avenir/627fbb5a-3bae-4cd9-b617-2f923e29d55e.woff2";
+const AvenirDemiBold = "/avenir/6afe4676-059a-4aa8-b891-29856bbcba22.woff2";
+const AvenirBold = "/avenir/14c73713-e4df-4dba-933b-057feeac8dd1.woff2";
+const AvenirHeavy = "/avenir/5c57b2e2-f641-421e-a95f-65fcb47e409a.woff2";
 
 /**
  * Colors
  * @tutorial https://docs.symbols.app/color
  */
 const COLOR = {
-  transparent: 'transparent',
-  black: 'black',
-  white: 'white',
+  transparent: "transparent",
+  black: "black",
+  white: "white",
 
-  blue: '#2485F0',
-  indigo: '#50E1FF',
-  green: '#59AC56',
-  red: '#EB6650',
-  yellow: '#E8CC56',
-  orange: '#E98232',
-  brown: '#7D6755',
-  pink: '#EE90BD',
-  purple: '#7345AF',
-  phosphorus: '#BCD835',
+  blue: "#2485F0",
+  indigo: "#50E1FF",
+  green: "#59AC56",
+  red: "#EB6650",
+  yellow: "#E8CC56",
+  orange: "#E98232",
+  brown: "#7D6755",
+  pink: "#EE90BD",
+  purple: "#7345AF",
+  phosphorus: "#BCD835",
   indigoblue: `#55B7D8`,
-  milk: '#D8D8D8',
-  grassgreen: '#6A9A55',
+  milk: "#D8D8D8",
+  grassgreen: "#6A9A55",
 
-  gray: '#57575c',
-  gray1: '#0C0C0D',
-  gray2: '#141416',
-  gray3: '#1C1C1F',
-  gray4: '#252527',
-  gray5: '#3E3E41',
-  gray6: '#57575C',
-  gray7: '#818186',
-  gray8: '#A3A3A8',
-  gray9: '#BDBDC1',
-  gray10: '#cfcfd1',
-  gray11: '#e0e0e2',
-  gray12: '#e9e9ea',
-}
+  gray: "#57575c",
+  gray1: "#0C0C0D",
+  gray2: "#141416",
+  gray3: "#1C1C1F",
+  gray4: "#252527",
+  gray5: "#3E3E41",
+  gray6: "#57575C",
+  gray7: "#818186",
+  gray8: "#A3A3A8",
+  gray9: "#BDBDC1",
+  gray10: "#cfcfd1",
+  gray11: "#e0e0e2",
+  gray12: "#e9e9ea",
+};
 
 /**
  * Gradients
@@ -58,35 +57,35 @@ const GRADIENT = {
   dotsDark: `radial-gradient(#252527 6%, transparent 6%)`,
   dotsLight: `radial-gradient(#cfcfd1 6%, transparent 6%)`,
 
-  'gradient-dark': `linear-gradient(0deg,
+  "gradient-dark": `linear-gradient(0deg,
     rgba(0,0,0,0.06) 0%,
     rgba(0,0,0,0.07) 100%
   )`,
-  'gradient-light': `linear-gradient(
+  "gradient-light": `linear-gradient(
     0deg,
     rgba(255,255,255,0.05) 0%,
     rgba(255,255,255,0.06) 100%
   )`,
-  'gradient-light-active': `linear-gradient(
+  "gradient-light-active": `linear-gradient(
     0deg,
     rgba(255,255,255,0.09) 0%,
     rgba(255,255,255,0.10) 100%
   )`,
-  'gradient-colorful': `linear-gradient(60deg,
+  "gradient-colorful": `linear-gradient(60deg,
     #00A2E7 0%,
     #185DF3 31%,
     #1E54F0 36%,
     #8B4CCA 69%,
     #C66894 100%
   )`,
-  'gradient-colorful-active': `linear-gradient(60deg,
+  "gradient-colorful-active": `linear-gradient(60deg,
     #00A2E7 0%,
     #185DF3 25%,
     #1E54F0 32%,
     #8B4CCA 75%,
     #C66894 100%
-  )`
-}
+  )`,
+};
 
 /**
  * Themes
@@ -96,121 +95,128 @@ const THEME = {
   transparent: {
     variants: {
       dark: {
-        background: 'transparent',
-        color: 'white 0.65'
+        background: "transparent",
+        color: "white 0.65",
       },
       light: {
-        background: 'transparent',
-        color: 'black 0.65'
-      }
-    }
+        background: "transparent",
+        color: "black 0.65",
+      },
+    },
   },
 
   text: {
-    background: 'transparent 0',
+    background: "transparent 0",
     variants: {
       dark: {
-        color: 'gray9'
+        color: "gray9",
       },
       light: {
-        color: 'gray3'
-      }
-    }
+        color: "gray3",
+      },
+    },
   },
 
   document: {
     variants: {
       dark: {
-        color: 'gray9',
-        background: 'black'
+        color: "gray9",
+        background: "black",
       },
       light: {
-        background: 'gray12',
-        color: 'gray1'
-      }
-    }
+        background: "gray12",
+        color: "gray1",
+      },
+    },
   },
   primary: {
     variants: {
       dark: {
-        color: 'white',
-        background: 'gradient-colorful'
+        color: "white",
+        background: "gradient-colorful",
       },
       light: {
-        color: 'white',
-        background: 'gradient-colorful'
-      }
-    }
+        color: "white",
+        background: "gradient-colorful",
+      },
+    },
   },
 
   secondary: {
-    color: 'white',
-    background: 'blue'
+    color: "white",
+    background: "blue",
   },
 
   tertiary: {
-    color: 'black',
-    background: 'white'
+    color: "black",
+    background: "white",
   },
 
   quaternary: {
     state: {
       hover: {
-        color: 'white',
-        background: 'gradient-light-active'
+        color: "white",
+        background: "gradient-light-active",
       },
       active: {
-        color: 'white',
-        background: 'gradient-light-active'
-      }
+        color: "white",
+        background: "gradient-light-active",
+      },
     },
     variants: {
       dark: {
-        color: 'gray9',
-        background: 'gradient-light'
+        color: "gray9",
+        background: "gradient-light",
       },
       light: {
-        color: 'gray1',
-        background: 'gradient-dark'
+        color: "gray1",
+        background: "gradient-dark",
       },
       active: {
-        color: 'white',
-        background: 'gradient-light-active'
-      }
-    }
+        color: "white",
+        background: "gradient-light-active",
+      },
+    },
   },
 
   quinary: {
-    background: 'white 0.05',
-    color: 'white 0.35'
-  }
-}
+    background: "white 0.05",
+    color: "white 0.35",
+  },
+};
 
 /**
  * Fonts
  * @tutorial https://docs.symbols.app/font
  */
 const FONT = {
-  smbls: [{
-    url: S3_BUCKET + AvenirLight,
-    fontWeight: 300
-  }, {
-    url: S3_BUCKET + AvenirRegular,
-    fontWeight: 400
-  }, {
-    url: S3_BUCKET + AvenirMedium,
-    fontWeight: 500
-  }, {
-    url: S3_BUCKET + AvenirDemiBold,
-    fontWeight: 600
-  }, {
-    url: S3_BUCKET + AvenirBold,
-    fontWeight: 700
-  }, {
-    url: S3_BUCKET + AvenirHeavy,
-    fontWeight: 900
-  }]
-}
+  smbls: [
+    {
+      url: S3_BUCKET + AvenirLight,
+      fontWeight: 300,
+    },
+    {
+      url: S3_BUCKET + AvenirRegular,
+      fontWeight: 400,
+    },
+    {
+      url: S3_BUCKET + AvenirMedium,
+      fontWeight: 500,
+    },
+    {
+      url: S3_BUCKET + AvenirDemiBold,
+      fontWeight: 600,
+    },
+    {
+      url: S3_BUCKET + AvenirBold,
+      fontWeight: 700,
+    },
+    {
+      url: S3_BUCKET + AvenirHeavy,
+      fontWeight: 900,
+    },
+  ],
+};
 
 /**
  * Font families
@@ -220,13 +226,13 @@ const FONT_FAMILY = {
   smbls: {
     isDefault: true,
     value: ['"smbls"'],
-    type: 'serif'
+    type: "serif",
   },
   code: {
     value: ['"Droid Sans Mono for Powerline"'],
-    type: 'monospace'
-  }
-}
+    type: "monospace",
+  },
+};
 
 /**
  * Typography
@@ -237,20 +243,20 @@ const TYPOGRAPHY = {
   base: 18,
   ratio: 1.2,
 
-  '@screenM': {
-    base: 16
+  "@screenM": {
+    base: 16,
   },
 
-  '@tabletM': {
+  "@tabletM": {
     base: 15,
-    ratio: 1.125
+    ratio: 1.125,
   },
 
-  '@mobileM': {
+  "@mobileM": {
     base: 14,
-    ratio: 1.125
-  }
-}
+    ratio: 1.125,
+  },
+};
 
 /**
  * Spacing
@@ -258,13 +264,18 @@ const TYPOGRAPHY = {
  * @tutorial https://docs.symbols.app/sequence
  */
 const SPACING = {
-  '@tabletM': {
-    ratio: 1.5
+  "@tabletM": {
+    ratio: 1.5,
   },
-  '@mobileM': {
-    ratio: 1.333
-  }
-}
+  "@mobileM": {
+    ratio: 1.333,
+  },
+};
+
+const MEDIA = {
+  mobile: "(max-width: 480px)",
+  tablet: "(max-width: 375px)",
+};
 
 init({
   COLOR,
@@ -273,5 +284,6 @@ init({
   TYPOGRAPHY,
   SPACING,
   FONT,
-  FONT_FAMILY
-})
+  FONT_FAMILY,
+  MEDIA,
+});
